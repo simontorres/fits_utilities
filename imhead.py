bc9090e4bdc42a231c76e15b2d53839f85a870fd
Simon Torres <storres@ctio.noao.edu>
2016-11-09 18:44:52 -0300

    fixed problem related to the difference of calling the program from pycharm or the terminal

--- imhead.py
+++ imhead.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/python2.7
 """Utility to read headers from FITS files
 
 This tool is intended to be used from a terminal in order to have easy access
@@ -25,30 +25,41 @@ class ImageHeader(object):
         Also performs some checks on the validity of the input
         """
         self.args = sys.argv
+        print self.args
         try:
-            self.file_list = glob.glob(self.args[1])
-            self.keywords = self.args[2:]
+            if '*' not in self.args[1]:
+                # if the program is called from a terminal
+                self.file_list = [arg for arg in self.args if '.fits' in arg]
+                self.keywords = [arg for arg in self.args[1:] if '.fits' not in arg]
+            else:
+                # if the program is called from pycharm
+                self.file_list = glob.glob(self.args[1])
+                self.keywords = self.args[2:]
             if self.file_list == []:
                 print('Error: No images to get header')
                 self.usage_exit()
-            if self.keywords == []:
-                print('Error: No keywords requested')
-                self.usage_exit()
+            # if self.keywords == []:
+                # print('Error: No keywords requested')
+                # self.usage_exit()
         except IndexError:
             self.usage_exit()
 
     def __call__(self):
         """Get header and print results"""
         for image in self.file_list:
-            image_info = "%s\t" % image
+            image_info = "%s" % image
             try:
                 header = fits.getheader(image)
-                for key in self.keywords:
-                    image_info += "%s\t" % header[key]
+                if self.keywords == []:
+                    dimensions = [header['NAXIS%s' % i] for i in range(1, int(header['NAXIS']) + 1, 1)]
+                    image_info += "%s: %s" %(dimensions, header['OBJECT'])
+                else:
+                    for key in self.keywords:
+                        image_info += "\t%s" % header[key]
             except IOError:
                 print("Image %s is corrupt or doesn't have a header" % image)
             except KeyError as err:
-                image_info += "<KeyError %s> \t" % key.upper()
+                image_info += "\t<KeyError %s>" % key.upper()
             print image_info
 
     @staticmethod
